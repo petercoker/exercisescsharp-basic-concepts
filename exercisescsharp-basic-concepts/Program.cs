a2abceb5ac8f433d14173cb1f1db29265fd49987
petercoker <petercoker@protonmail.com>
2019-01-12 16:53:48 +0000

    first init

---
+++ exercisescsharp-basic-concepts/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace exercises_csharp_basic_concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello_world hello = new Hello_world();
+            hello.PrintHelloWorld();
+            
+        }
+    }
+}
---
+++ exercisescsharp-basic-concepts/hello_world.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace exercises_csharp_basic_concepts
+{
+    class Hello_world
+    {
+        public void PrintHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
