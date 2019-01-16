a92e83bbe9777bb848cd5d2eead75ab440b542bb
petercoker <petercoker@protonmail.com>
2019-01-16 13:02:24 +0000

    Created divison class

---
+++ exercisescsharp-basic-concepts/Division.cs
@@ -0,0 +1,13 @@
+using System;
+using exercisescsharp_basic_concepts;
+
+namespace exercises_csharp_basic_concepts
+{
+    public class Division
+    {
+        public int GetDivide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
