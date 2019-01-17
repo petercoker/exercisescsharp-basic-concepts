e271387e33f9b88fb55ca5c44a9fa1b84c7828a1
petercoker <petercoker@protonmail.com>
2019-01-17 00:23:55 +0000

    added remainder class

---
+++ exercisescsharp-basic-concepts/Remainder.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Runtime.InteropServices;
+using exercisescsharp_basic_concepts;
+
+namespace exercises_csharp_basic_concepts
+{
+    class Remainder
+    {
+        
+    }
+}
