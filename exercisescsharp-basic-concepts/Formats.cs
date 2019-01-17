e9eeebb1d84bcd3233369af9a3c63c7fba145798
petercoker <petercoker@protonmail.com>
2019-01-17 23:58:37 +0000

    created format class

---
+++ exercisescsharp-basic-concepts/Formats.cs
@@ -0,0 +1,14 @@
+//+src=/sdcard/Git/exercisescsharp-basic-concepts/Program.cs
+using System;
+using System.Runtime.InteropServices;
+
+namespace exercises_csharp_basic_concepts
+{
+    class Format
+    {
+        public void GetRemainder(string name)
+        {
+           Console.WriteLine
+        }
+    }
+}
