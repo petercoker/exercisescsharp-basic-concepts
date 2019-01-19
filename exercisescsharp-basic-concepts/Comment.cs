758b121582601a5127d883a20481131ba8f3b7fe
Peter Coker <petercoker@protonmail.com>
2019-01-19 20:12:34 +0000

    update

---
+++ exercisescsharp-basic-concepts/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercisescsharp_basic_concepts
+{
+    class Comment
+    {
+        public void Hide()
+        {
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Hello " + name + "!\n");
+            /*
+            Console.WriteLine($"Hello {name}!");
+            Console.WriteLine("Hello {0}!", name);
+            */
+        }
+    }
+}
--- exercisescsharp-basic-concepts/Program.cs
+++ exercisescsharp-basic-concepts/Program.cs
@@ -1,15 +1,16 @@
 //+src=/sdcard/Git/exercisescsharp-basic-concepts/Remainder.cs
 using System;
 using System.Runtime.InteropServices;
- 
+using exercisescsharp_basic_concepts;
+
 namespace exercises_csharp_basic_concepts
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Formats format = new Formats();
-            format.PrintName();
+            Comment format = new Comment();
+            format.Hide();
 
         }
     }
