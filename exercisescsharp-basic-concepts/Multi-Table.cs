25e7a18af5f41f423bc0f0553a2d48b88556c5e6
Peter Coker <petercoker@protonmail.com>
2019-01-24 13:50:07 +0000

    create multi table class

---
+++ exercisescsharp-basic-concepts/Multi-Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercisescsharp_basic_concepts
+{
+    class Multi_Table
+    {
+        public void Multi(int num)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                int sum = i * num;
+                Console.WriteLine("{0} x {1} = {2}", num, i, sum);
+            }
+        }
+    }
+}
--- exercisescsharp-basic-concepts/Program.cs
+++ exercisescsharp-basic-concepts/Program.cs
@@ -9,15 +9,13 @@ namespace exercises_csharp_basic_concepts
     {
         static void Main(string[] args)
         {
-            
-            BasicCal basic = new BasicCal();
+            int num;
+            Multi_Table basic = new Multi_Table();
 
             Console.Write("Enter a number: ");
-            int num1 = int.Parse(Console.ReadLine());
-            basic.GetCalAdd(num1);
-            basic.GetCalSub(num1);
-            basic.GetCalMul(num1);
-            basic.GetCalDiv(num1);
+            num = int.Parse(Console.ReadLine());
+            basic.Multi(num);
+
         }
     }
 }
