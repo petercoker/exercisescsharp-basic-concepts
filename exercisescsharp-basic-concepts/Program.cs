c74af6a88dd53d040ee905b3b0116d28c86efb76
Peter Coker <petercoker@protonmail.com>
2019-02-04 22:07:07 +0000

    average

--- exercisescsharp-basic-concepts/Program.cs
+++ exercisescsharp-basic-concepts/Program.cs
@@ -1,6 +1,5 @@
 //+src=/sdcard/Git/exercisescsharp-basic-concepts/Remainder.cs
 using System;
-using System.Runtime.InteropServices;
 using exercisescsharp_basic_concepts;
 
 namespace exercises_csharp_basic_concepts
@@ -9,12 +8,8 @@ namespace exercises_csharp_basic_concepts
     {
         static void Main(string[] args)
         {
-            int num;
-            Multi_Table basic = new Multi_Table();
-
-            Console.Write("Enter a number: ");
-            num = int.Parse(Console.ReadLine());
-            basic.Multi(num);
+            Average average = new Average();
+            average.GetAverage();
 
         }
     }
---
+++ exercisescsharp-basic-concepts/average.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exercisescsharp_basic_concepts
+{
+    class Average
+    {
+        public void GetAverage()
+        {
+            double average = 0;
+            double number;
+            int count = 1;
+
+            do
+            {
+                Console.Write($"Enter a number {count}/4: ");
+                number = double.Parse(Console.ReadLine());
+                average += number;
+
+                count++;
+            } while (count <= 4);
+
+            Console.WriteLine($"Average is {average}");
+            
+        }
+    }
+}
